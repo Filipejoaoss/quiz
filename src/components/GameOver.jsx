42751360bf73af437e6e01a6514e0ffc89f617c7
Filipe Sousa <seica.filipejoao@gmail.com>
2023-09-18 15:31:51 +0100

    Page Game Over done

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import "./GameOver.css";
+
+import WellDone from "../img/welldone.svg";
+
+const GameOver = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div id="gameover">
+      <h2>Game over!</h2>
+      <p>Score: { quizState.score } </p>
+      <p>You got { quizState.score } out of { quizState.questions.length } {" "} questions right!</p>
+      <img src={WellDone} alt="Game over" />
+      <button>Restart</button>
+    </div>
+  );
+};
+
+export default GameOver;
